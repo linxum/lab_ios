90f009ed176a5be7cf0614f4bef39665a83d72e7
linxum <ivansmehnev@gmail.com>
2023-10-30 23:02:40 +0300

    Added functions from the 2nd Lecture (some nonsense)

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -2,24 +2,42 @@
 //  ViewController.swift
 //  Concentration
 //
-//  Created by Дебилы on 17.10.2023.
-//  Copyright © 2023 Дебилы. All rights reserved.
+//  Created by Дебилы Entertainment on 03.10.2023.
+//  Copyright © 2023 Дебилы Entertainment. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    var flipCount = 0 {
+        didSet {
+            flipCountLabel.text = "Score: \(flipCount)"
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    var emojiChoices = ["🍒", "🍉", "🍌", "🍊"]
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        if let cardNumber = cardButtons.index(of: sender) {
+            flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+        }
     }
-
-
+    
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControlState.normal)
+            button.backgroundColor = UIColor.gray
+        } else {
+            button.setTitle(emoji, for: UIControlState.normal)
+            button.backgroundColor = UIColor.white
+        }
+    }
 }
 
