7591c7390fd929faea17188237dbd5d6d4eaa6e1
linxum <ivansmehnev@gmail.com>
2023-11-17 13:10:22 +0300

    Added file for themes and difficulty

---
+++ Concentration/Mods.swift
@@ -0,0 +1,18 @@
+//
+//  Mods.swift
+//  Concentration
+//
+//  Created by Дебилы on 15.11.2023.
+//  Copyright © 2023 Дебилы. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Mods: UIViewController, UISplitViewControllerDelegate {
+    let themes = [
+        "Food": "🍒🍉🍌🍊🍇🍑🥝🍏🍋🍓🌶🍕",
+        "Animals": "🐶🐱🦊🐴🐼🐨🦁🐷🦆🦄🦉🦇",
+        "Flags": "🇷🇺🇺🇦🇧🇾🇰🇿🇹🇲🇺🇿🇹🇯🇰🇬🇦🇿🇦🇲🇬🇪🇲🇩"
+    ]
+}
